c16fa84fd6ef053b1f19e04c298491fd19eac841
marijane white <whimar@ohsu.edu>
2019-07-05 12:41:52 -0700

    Update to support POST requests for Blazegraph
    Add:
    * optional "post" parameter that defaults to False
    * conditional code to support POST requests for Blazegraph

--- sparql_dataframe/sparql_dataframe.py
+++ sparql_dataframe/sparql_dataframe.py
@@ -4,18 +4,26 @@ Query a SPARQL endpoint and return results as a Pandas dataframe.
 
 import pandas as pd
 
-from SPARQLWrapper import SPARQLWrapper, CSV, SELECT
+from SPARQLWrapper import SPARQLWrapper, CSV, SELECT, POST, POSTDIRECTLY
 
 
 class QueryException(Exception):
     pass
 
 
-def get_sparql_dataframe(endpoint, query):
+def get_sparql_dataframe(endpoint, query, post=False):
     sparql = SPARQLWrapper(endpoint)
     sparql.setQuery(query)
     if sparql.queryType != SELECT:
         raise QueryException("Only SPARQL SELECT queries are supported.")
+
+    if post:
+        sparql.setOnlyConneg(True)
+        sparql.addCustomHttpHeader("Content-type", "application/sparql-query")
+        sparql.addCustomHttpHeader("Accept", "text/csv")
+        sparql.setMethod(POST)
+        sparql.setRequestMethod(POSTDIRECTLY)
+
     sparql.setReturnFormat(CSV)
     results = sparql.query().convert()
     _csv = pd.compat.StringIO(results.decode('utf-8'))
