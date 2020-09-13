225016fdcce88e25b405033691b048665f8822ec
Michael Chary <mac389@gmail.com>
2020-09-13 15:54:46 -0400

    update

---
+++ build/lib/sparql_dataframe/__init__.py
@@ -0,0 +1,2 @@
+from .sparql_dataframe import get_sparql_dataframe as get
+from .sparql_dataframe import QueryException
---
+++ build/lib/sparql_dataframe/sparql_dataframe.py
@@ -0,0 +1,31 @@
+"""
+Query a SPARQL endpoint and return results as a Pandas dataframe.
+"""
+
+import pandas as pd
+import io
+
+from SPARQLWrapper import SPARQLWrapper, CSV, SELECT, POST, POSTDIRECTLY
+
+
+class QueryException(Exception):
+    pass
+
+
+def get_sparql_dataframe(endpoint, query, post=False):
+    sparql = SPARQLWrapper(endpoint)
+    sparql.setQuery(query)
+    if sparql.queryType != SELECT:
+        raise QueryException("Only SPARQL SELECT queries are supported.")
+
+    if post:
+        sparql.setOnlyConneg(True)
+        sparql.addCustomHttpHeader("Content-type", "application/sparql-query")
+        sparql.addCustomHttpHeader("Accept", "text/csv")
+        sparql.setMethod(POST)
+        sparql.setRequestMethod(POSTDIRECTLY)
+
+    sparql.setReturnFormat(CSV)
+    results = sparql.query().convert()
+    _csv = io.StringIO(results.decode('utf-8'))
+    return pd.read_csv(_csv, sep=",")
